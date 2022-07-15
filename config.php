f755e8a4616730914f56705a73a76199c52da46d
Waluisachi <philipwaluisachi@gmail.com>
2022-07-15 23:48:06 +0300

    Global configuration file

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/******* GLOBAL CONFIGURATIONS ********/
+// EDIT TO YOUR SITE CONFIGURATIONS
+
+
+Define('domain', 'http://'.$_SERVER['HTTP_HOST'].'/belt-render/');
+Define(DB_HOST, 'localhost');
+Define(DB_USERNAME, 'root');
+Define(DB_PASSWORD, '');
+Define(DB_NAME, 'engine');
+
+ ?>
