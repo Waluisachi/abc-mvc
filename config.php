14d92f444f7a3c3d3ef776753f8966d4c3a11da2
Waluisachi <philipwaluisachi@gmail.com>
2022-07-24 09:52:29 +0300

    Cleaning up dev configs

--- config.php
+++ config.php
@@ -4,10 +4,10 @@
 
 
 Define('domain', 'http://'.$_SERVER['HTTP_HOST'].'/belt-render/');
-Define('DB_HOST', 'localhost');
-Define('DB_TYPE', 'mysql');
-Define('DB_USERNAME', 'root');
+Define('DB_HOST', '');
+Define('DB_TYPE', '');
+Define('DB_USERNAME', '');
 Define('DB_PASSWORD', '');
-Define('DB_NAME', 'engine');
+Define('DB_NAME', '');
 
  ?>
